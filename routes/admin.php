dd0f3867693c4eb77bbdb50cefad0491985f38de
vannguyenv12 <npnv.vn1@gmail.com>
2024-02-14 14:32:51 +0700

    feat: Product Gallery

--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\AdminAuthController;
 use App\Http\Controllers\Admin\AdminDashboardController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\ProductGalleryController;
 use App\Http\Controllers\Admin\ProfileController;
 use App\Http\Controllers\Admin\SliderController;
 use App\Http\Controllers\Admin\WhyChooseUsController;
@@ -35,4 +36,9 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
 
     // Product Routes
     Route::resource('product', ProductController::class);
+
+    // Product Gallery Routes
+    Route::get('product-gallery/{product}', [ProductGalleryController::class, 'index'])
+        ->name('product-gallery.show-index');
+    Route::resource('product-gallery', ProductGalleryController::class);
 });
