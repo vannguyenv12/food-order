6031dbd98215c8ecc9af202ad5c3cf891c6a4104
vannguyenv12 <npnv.vn1@gmail.com>
2024-02-28 11:18:05 +0700

    feat: Coupon (Completed CRUD on Backend)

--- app/Http/Controllers/Admin/CouponController.php
+++ app/Http/Controllers/Admin/CouponController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\DataTables\CouponDataTable;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\CouponCreateRequest;
+use App\Models\Coupon;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -32,6 +33,20 @@ class CouponController extends Controller
      */
     public function store(CouponCreateRequest $request): RedirectResponse
     {
+        $coupon = new Coupon();
+        $coupon->name = $request->name;
+        $coupon->code = $request->code;
+        $coupon->quantity = $request->quantity;
+        $coupon->min_purchase_amount = $request->min_purchase_amount;
+        $coupon->expire_date = $request->expire_date;
+        $coupon->discount_type = $request->discount_type;
+        $coupon->discount = $request->discount;
+        $coupon->status = $request->status;
+
+        $coupon->save();
+
+        toastr()->success('Created Successfully!');
+
         return to_route('admin.coupon.index');
     }
 
@@ -46,17 +61,34 @@ class CouponController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id): View
     {
-        //
+        $coupon = Coupon::findOrFail($id);
+
+        return view('admin.coupon.edit', compact('coupon'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(CouponCreateRequest $request, string $id)
     {
-        //
+        $coupon = Coupon::findOrFail($id);
+
+        $coupon->name = $request->name;
+        $coupon->code = $request->code;
+        $coupon->quantity = $request->quantity;
+        $coupon->min_purchase_amount = $request->min_purchase_amount;
+        $coupon->expire_date = $request->expire_date;
+        $coupon->discount_type = $request->discount_type;
+        $coupon->discount = $request->discount;
+        $coupon->status = $request->status;
+
+        $coupon->save();
+
+        toastr()->success('Updated Successfully!');
+
+        return to_route('admin.coupon.index');
     }
 
     /**
@@ -64,6 +96,11 @@ class CouponController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            Coupon::findOrFail($id)->delete();
+            return response(['status' => 'success', 'message' => 'Deleted Successfully!']);
+        } catch (\Exception $e) {
+            return response(['status' => 'error', 'message' => 'Something went wrong!']);
+        }
     }
 }
