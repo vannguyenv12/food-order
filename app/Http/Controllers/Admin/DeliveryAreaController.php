9dce69fa0a161098710f6b2c773e4fac85272bcd
vannguyenv12 <npnv.vn1@gmail.com>
2024-03-04 21:34:51 +0700

    feat: Delivery Area (Create)

---
+++ app/Http/Controllers/Admin/DeliveryAreaController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\DataTables\DeliveryAreaDataTable;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\DeliveryAreaCreateRequest;
+use App\Models\DeliveryArea;
+use Illuminate\Http\Request;
+
+class DeliveryAreaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(DeliveryAreaDataTable $dataTable)
+    {
+        return $dataTable->render('admin.delivery-area.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.delivery-area.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(DeliveryAreaCreateRequest $request)
+    {
+        $area = new DeliveryArea();
+        $area->area_name = $request->area_name;
+        $area->min_delivery_time = $request->min_delivery_time;
+        $area->max_delivery_time = $request->max_delivery_time;
+        $area->delivery_fee = $request->delivery_fee;
+        $area->status = $request->status;
+
+        $area->save();
+
+        toastr()->success('Created Successfully!');
+
+        return to_route('admin.delivery-area.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\AdminAuthController;
 use App\Http\Controllers\Admin\AdminDashboardController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\CouponController;
+use App\Http\Controllers\Admin\DeliveryAreaController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\ProductGalleryController;
 use App\Http\Controllers\Admin\ProductOptionController;
@@ -56,6 +57,9 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
 
     // Coupon Routes
     Route::resource('coupon', CouponController::class);
+
+    // Delivery Area
+    Route::resource('delivery-area', DeliveryAreaController::class);
 
     // Setting Route
     Route::get('/setting', [SettingController::class, 'index'])->name('setting.index');
