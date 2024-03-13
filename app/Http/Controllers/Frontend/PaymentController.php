e742e152db439fe76cb7033a904d139b67702d66
vannguyenv12 <npnv.vn1@gmail.com>
2024-03-13 20:18:50 +0700

    feat: Payment Gateway (Override Configuration)

--- app/Http/Controllers/Frontend/PaymentController.php
+++ app/Http/Controllers/Frontend/PaymentController.php
@@ -41,6 +41,31 @@ class PaymentController extends Controller
         }
     }
 
+    private function setPaypalConfig()
+    {
+        $config = [
+            'mode'    => config('gatewaySettings.paypal_account_mode'), // Can only be 'sandbox' Or 'live'. If empty or invalid, 'live' will be used.
+            'sandbox' => [
+                'client_id'         => config('gatewaySettings.paypal_api_key'),
+                'client_secret'     => config('gatewaySettings.paypal_secret_id'),
+                'app_id'            => 'APP-80W284485P519543T',
+            ],
+            'live' => [
+                'client_id'         => config('gatewaySettings.paypal_api_key'),
+                'client_secret'     => config('gatewaySettings.paypal_secret_id'),
+                'app_id'            => env('PAYPAL_LIVE_APP_ID', ''),
+            ],
+
+            'payment_action' => 'Sale', // Can only be 'Sale', 'Aut horization' or 'Order'
+            'currency'       => config('gatewaySettings.paypal_currency'),
+            'notify_url'     => env('PAYPAL_NOTIFY_URL', ''), // Change this accordingly for your application.
+            'locale'         =>  'en_US', // force gateway language  i.e. it_IT, es_ES, en_US ... (for express checkout only)
+            'validate_ssl'   => true, // Validate SSL when creating api client.
+        ];
+
+        return $config;
+    }
+
     public function payWithPaypal()
     {
         return "Payment Processing";
