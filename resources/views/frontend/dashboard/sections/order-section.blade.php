2c24fcd010e17fe20ef0e0da63297885ede128f5
vannguyenv12 <npnv.vn1@gmail.com>
2024-03-18 23:11:23 +0700

    feat: Order (Show Detail Order)

--- resources/views/frontend/dashboard/sections/order-section.blade.php
+++ resources/views/frontend/dashboard/sections/order-section.blade.php
@@ -46,27 +46,41 @@
 
         @foreach ($orders as $order)
             <div class="fp__invoice invoice_details_{{ $order->id }}">
-                <a class="go_back"><i class="fas fa-long-arrow-alt-left"></i> go back</a>
-                <div class="fp__track_order">
+                <a class="go_back d-print-none"><i class="fas fa-long-arrow-alt-left"></i> go back</a>
+                <div class="fp__track_order d-print-none">
                     <ul>
-                        <li class="active">order pending</li>
-                        <li>order accept</li>
-                        <li>order process</li>
-                        <li>on the way</li>
-                        <li>Completed</li>
+                        @if ($order->order_status === 'declined_status')
+                            <li class="{{ in_array($order->order_status, ['declined']) ? 'active' : '' }}">
+                                order declined</li>
+                        @else
+                            <li
+                                class="
+                        {{ in_array($order->order_status, ['pending', 'in_process', 'delivered', 'declined']) ? 'active' : '' }}">
+                                order pending</li>
+                            <li
+                                class="{{ in_array($order->order_status, ['in_process', 'delivered', 'declined']) ? 'active' : '' }}">
+                                order in process</li>
+
+                            <li class="{{ in_array($order->order_status, ['delivered']) ? 'active' : '' }}">
+                                order delivered</li>
+                        @endif
                     </ul>
                 </div>
                 <div class="fp__invoice_header">
                     <div class="header_address">
                         <h4>invoice to</h4>
-                        <p>7232 Broadway Suite 308, Jackson Heights, 11372, NY, United
-                            States</p>
-                        <p>+1347-430-9510</p>
+                        <p>{{ @$order->userAddress->first_name }}</p>
+                        <p>{{ @$order->address }}</p>
+                        <p>{{ @$order->userAddress->phone }}</p>
+                        <p>{{ @$order->userAddress->email }}</p>
                     </div>
                     <div class="header_address">
-                        <p><b>invoice no: </b><span>4574</span></p>
-                        <p><b>Order ID:</b> <span> #4789546458</span></p>
-                        <p><b>date:</b> <span>10-11-2022</span></p>
+                        <p><b>invoice no: </b><span>{{ @$order->invoice_id }}</span></p>
+                        <p><b>Payment Status:</b> <span> {{ @$order->payment_status }}</span></p>
+                        <p><b>Payment Method:</b> <span> {{ @$order->payment_method }}</span></p>
+                        <p><b>Transaction Id:</b> <span> {{ @$order->transaction_id }}</span></p>
+
+                        <p><b>date:</b> <span>{{ date('d-m-Y', strtotime($order->created_at)) }}</span></p>
                     </div>
                 </div>
                 <div class="fp__invoice_body">
@@ -80,128 +94,45 @@
                                     <th class="qnty">Quantity</th>
                                     <th class="total">Total</th>
                                 </tr>
-                                <tr>
-                                    <td class="sl_no">01</td>
-                                    <td class="package">
-                                        <p>Hyderabadi Biryani</p>
-                                        <span class="size">small</span>
-                                        <span class="coca_cola">coca-cola</span>
-                                        <span class="coca_cola2">7up</span>
-                                    </td>
-                                    <td class="price">
-                                        <b>$120</b>
-                                    </td>
-                                    <td class="qnty">
-                                        <b>2</b>
-                                    </td>
-                                    <td class="total">
-                                        <b>$240</b>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="sl_no">02</td>
-                                    <td class="package">
-                                        <p>Daria Shevtsova</p>
-                                        <span class="size">medium</span>
-                                        <span class="coca_cola">coca-cola</span>
-                                    </td>
-                                    <td class="price">
-                                        <b>$120</b>
-                                    </td>
-                                    <td class="qnty">
-                                        <b>2</b>
-                                    </td>
-                                    <td class="total">
-                                        <b>$240</b>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="sl_no">03</td>
-                                    <td class="package">
-                                        <p>Hyderabadi Biryani</p>
-                                        <span class="size">large</span>
-                                        <span class="coca_cola2">7up</span>
-                                    </td>
-                                    <td class="price">
-                                        <b>$120</b>
-                                    </td>
-                                    <td class="qnty">
-                                        <b>2</b>
-                                    </td>
-                                    <td class="total">
-                                        <b>$240</b>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="sl_no">04</td>
-                                    <td class="package">
-                                        <p>Hyderabadi Biryani</p>
-                                        <span class="size">medium</span>
-                                        <span class="coca_cola">coca-cola</span>
-                                        <span class="coca_cola2">7up</span>
-                                    </td>
-                                    <td class="price">
-                                        <b>$120</b>
-                                    </td>
-                                    <td class="qnty">
-                                        <b>2</b>
-                                    </td>
-                                    <td class="total">
-                                        <b>$240</b>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="sl_no">05</td>
-                                    <td class="package">
-                                        <p>Daria Shevtsova</p>
-                                        <span class="size">large</span>
-                                    </td>
-                                    <td class="price">
-                                        <b>$120</b>
-                                    </td>
-                                    <td class="qnty">
-                                        <b>2</b>
-                                    </td>
-                                    <td class="total">
-                                        <b>$240</b>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="sl_no">04</td>
-                                    <td class="package">
-                                        <p>Hyderabadi Biryani</p>
-                                        <span class="size">medium</span>
-                                        <span class="coca_cola">coca-cola</span>
-                                        <span class="coca_cola2">7up</span>
-                                    </td>
-                                    <td class="price">
-                                        <b>$120</b>
-                                    </td>
-                                    <td class="qnty">
-                                        <b>2</b>
-                                    </td>
-                                    <td class="total">
-                                        <b>$240</b>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="sl_no">04</td>
-                                    <td class="package">
-                                        <p>Hyderabadi Biryani</p>
-                                        <span class="size">medium</span>
-                                        <span class="coca_cola">coca-cola</span>
-                                        <span class="coca_cola2">7up</span>
-                                    </td>
-                                    <td class="price">
-                                        <b>$120</b>
-                                    </td>
-                                    <td class="qnty">
-                                        <b>2</b>
-                                    </td>
-                                    <td class="total">
-                                        <b>$240</b>
-                                    </td>
-                                </tr>
+
+                                @foreach ($order->orderItems as $item)
+                                    @php
+                                        $size = json_decode($item->product_size);
+                                        $options = json_decode($item->product_option);
+
+                                        $qty = $item->qty;
+                                        $unitPrice = $item->unit_price;
+                                        $sizePrice = $size->price ?? 0;
+                                        $optionPrice = 0;
+                                        foreach ($options as $optionItem) {
+                                            $optionPrice += $optionItem->price;
+                                        }
+
+                                        $productTotal = ($unitPrice + $sizePrice + $optionPrice) * $qty;
+                                    @endphp
+                                    <tr>
+                                        <td class="sl_no">{{ ++$loop->index }}</td>
+                                        <td class="package">
+                                            <p>{{ $item->product_name }}</p>
+                                            <span class="size">{{ @$size->name }} -
+                                                {{ @$size->price ? currencyPosition(@$size->price) : '' }}</span>
+                                            @foreach ($options as $option)
+                                                <span class="coca_cola">{{ @$option['name'] }} -
+                                                    {{ @$option['price'] ? currencyPosition($option['price']) : '' }}</span>
+                                            @endforeach
+                                        </td>
+                                        <td class="price">
+                                            <b>{{ currencyPosition($item->unit_price) }}</b>
+                                        </td>
+                                        <td class="qnty">
+                                            <b>{{ $item->qty }}</b>
+                                        </td>
+                                        <td class="total">
+                                            <b>{{ currencyPosition($productTotal) }}</b>
+                                        </td>
+                                    </tr>
+                                @endforeach
+
                             </tbody>
                             <tfoot>
                                 <tr>
@@ -209,10 +140,10 @@
                                         <b>sub total</b>
                                     </td>
                                     <td class="qnty">
-                                        <b>12</b>
+                                        <b>-</b>
                                     </td>
                                     <td class="total">
-                                        <b>$755</b>
+                                        <b>{{ currencyPosition($order->subtotal) }}</b>
                                     </td>
                                 </tr>
                                 <tr>
@@ -223,7 +154,7 @@
                                         <b></b>
                                     </td>
                                     <td class="total coupon">
-                                        <b>$0.00</b>
+                                        <b>{{ currencyPosition($order->discount) }}</b>
                                     </td>
                                 </tr>
                                 <tr>
@@ -234,7 +165,7 @@
                                         <b></b>
                                     </td>
                                     <td class="total coast">
-                                        <b>$10.00</b>
+                                        <b>{{ currencyPosition($order->delivery_charge) }}</b>
                                     </td>
                                 </tr>
                                 <tr>
@@ -245,14 +176,15 @@
                                         <b></b>
                                     </td>
                                     <td class="total">
-                                        <b>$765</b>
+                                        <b>{{ currencyPosition($order->grand_total) }}</b>
                                     </td>
                                 </tr>
                             </tfoot>
                         </table>
                     </div>
                 </div>
-                <a class="print_btn common_btn" href="#"><i class="far fa-print"></i>
+                <a class="print_btn common_btn d-print-none" href="javascript:;"
+                    onclick="printInvoice('{{ $order->id }}')"><i class="far fa-print"></i>
                     print
                     PDF</a>
 
@@ -266,6 +198,25 @@
         function viewInvoice(id) {
             $(".fp_dashboard_order").fadeOut();
             $(".invoice_details_" + id).fadeIn();
+        }
+
+        function printInvoice(id) {
+            let printContent = $('.invoice_details_' + id).html();
+
+            let printWindow = window.open('', '', 'width=600, height=600');
+            printWindow.document.open();
+            printWindow.document.write('<html>');
+            printWindow.document.write(
+                `    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}">
+                                `);
+            printWindow.document.write('<body>');
+
+            printWindow.document.write(printContent);
+            printWindow.document.write(
+                '</body></html>');
+            printWindow.document.close();
+
+            printWindow.print();
         }
     </script>
 @endpush
